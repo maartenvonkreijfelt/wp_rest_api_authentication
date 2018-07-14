765da49d539ced4231cf92e31d862194a8fc3950
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-07-14 14:22:05 +0200

    Update

--- wp-content/plugins/restedit/js/restedit.ajax.js
+++ wp-content/plugins/restedit/js/restedit.ajax.js
@@ -1,10 +1,34 @@
 (function($) {
 
-console.info("The REST API root is:", WPsettings.root);
+	// Get the entry tile element:
+	const $ENTRYTITLE = $('.entry-title');
 
-	console.info("The nonce value is:", WPsettings.nonce);
+	// Add an edit button and a save button directly after the entry title.
+	// Hide the save button using display:none in an inline style.
+    $ENTRYTITLE.after( '<button class="edit-button edit-title">Edit title</button><button class="edit-title save" style="display: none">Save title</button>' );
 
-	console.info("The current post id :", WPsettings.current_ID);
+	$('.edit-title.edit-button').click(function(){
+		let $originalTitle = $ENTRYTITLE.text();
+		$ENTRYTITLE.toggle();
+		$ENTRYTITLE.after('<input id="title-input" type="text">');
+		document.querySelector('#title-input').value = $originalTitle;
+		$(this).toggle();
+		$('.edit-title.save').toggle();
+	});
+
+	$('.save').click(function(){
+		let newTitle = document.querySelector('#title-input').value;
+		$ENTRYTITLE.text(newTitle);
+		$ENTRYTITLE.toggle();
+		$('#title-input').toggle();
+		$('.edit-title.edit-button').toggle();
+		$(this).toggle();
+	});
+
+
+
+
+
 
 
 })(jQuery);
--- wp-content/plugins/restedit/restedit.php
+++ wp-content/plugins/restedit/restedit.php
@@ -3,8 +3,8 @@
 Plugin Name: Front-end editing via the REST API
 Description: Extend cookie authentication to the REST API to allow front-end editing.
 Version:     0.1
-Author:      Maarten von Kreijfelt
-Author URI:
+Author:      Morten Rand-Hendriksen
+Author URI:  http://lynda.com/mor10
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
